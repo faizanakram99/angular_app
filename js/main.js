5f24742810a1f633d75604701e48ec334dc2095c
faizanakram99 <faizanakram99@gmail.com>
2016-03-12 21:02:23 +0530

    Done All

--- js/main.js
+++ js/main.js
@@ -1,52 +1,43 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate', 'ui.date']);
 
 myApp.config(function ($routeProvider) {
+
     $routeProvider
         .when('/', {
             templateUrl: 'pages/login.html',
             controller: 'loginCtrl'
         })
         .when('/home', {
-                        resolve: {
-                            "check": function ($location, $rootScope) {
-                                if (!$rootScope.loggedIn) {
-                                    $location.path('/');
-                                }
-                            }
-                        },
+            resolve: {
+                "check": function ($location, $rootScope) {
+                    if (!$rootScope.loggedIn) {
+                        $location.path('/');
+                    }
+                }
+            },
             templateUrl: 'pages/home.html',
             controller: 'homeCtrl'
         })
         .when('/register', {
-                        resolve: {
-                            "check": function ($location, $rootScope) {
-                                if (!$rootScope.loggedIn) {
-                                    $location.path('/');
-                                }
-                            }
-                        },
+            resolve: {
+                "check": function ($location, $rootScope) {
+                    if (!$rootScope.loggedIn) {
+                        $location.path('/');
+                    }
+                }
+            },
             templateUrl: 'pages/register.html',
             controller: 'registerCtrl'
         })
-        .when('/complete', {
-                        resolve: {
-                            "check": function ($location, $rootScope) {
-                                if (!$rootScope.loggedIn) {
-                                    $location.path('/');
-                                }
-                            }
-                        },
-            templateUrl: 'pages/complete.html',
-            controller: 'completeCtrl'
-        })
-        .when('/profile', {
-                        resolve: {
-                            "check": function ($location, $rootScope) {
-                                if (!$rootScope.loggedIn) {
-                                    $location.path('/');
-                                }
-                            }
-                        },
+
+    .when('/profile', {
+            resolve: {
+                "check": function ($location, $rootScope) {
+                    if (!$rootScope.loggedIn) {
+                        $location.path('/');
+                    }
+                }
+            },
             templateUrl: 'pages/profile.html',
             controller: 'profileCtrl'
         })
@@ -110,7 +101,7 @@ myApp.controller('loginCtrl', function ($scope, $location, $rootScope, $http) {
 //home Controller starts
 myApp.controller('homeCtrl', function ($scope) {
     $scope.bg = "#74828F";
-   });
+});
 //home controller ends
 
 //register Controller Starts
@@ -118,13 +109,49 @@ myApp.controller('registerCtrl', function ($scope, $location, $http) {
 
     $scope.bg = "#96C0CE";
 
+
     $scope.dateOptions = {
         changeYear: true,
         changeMonth: true,
         yearRange: '1951:1998'
     };
 
+    $scope.checkEmail = function () {
+
+        return $http({
+                method: 'GET',
+                url: 'https://api.backand.com:443/1/objects/users',
+                headers: {
+                    'AnonymousToken': '9be2ef1b-0af7-4582-a0cb-45b0a8667739'
+                },
+                params: {
+                    pageSize: 1,
+                    pageNumber: 1,
+                    filter: [
+                        {
+                            fieldName: 'email',
+                            operator: 'equals',
+                            value: $scope.email
+      }
+    ],
+                    sort: ''
+                }
+            })
+            .then(function (response) {
+
+                    if ($scope.email === response.data.data[0].email) {
+                        $scope.emailError = "Email is already in use!";
+                        $scope.hideEmailError = false;
+                    }
+
+                },
+                function errorCallback(response) {
+                    alert('Could not connect to server');
+                });
+    };
+
     $scope.register = function () {
+
 
         $scope.isSaving = true;
 
@@ -145,13 +172,17 @@ myApp.controller('registerCtrl', function ($scope, $location, $http) {
                 'AnonymousToken': '9be2ef1b-0af7-4582-a0cb-45b0a8667739'
             }
         }).then(function () {
+
+                $location.path('/profile');
                 $scope.isSaving = false;
-                $location.path('/complete');
             },
             function errorCallback(response) {
                 alert('Could not connect to server');
                 $scope.isSaving = false;
+
             });
+
+
 
     };
 
@@ -161,18 +192,33 @@ myApp.controller('registerCtrl', function ($scope, $location, $http) {
 //Register Controller Ends
 
 
-//complete Controller starts
-myApp.controller('completeCtrl', function ($scope) {
-    $scope.bg = "#BEB9B5";
-
-});
-//complete controller ends
-
-
 //profile Controller starts
-myApp.controller('profileCtrl', function ($scope) {
+myApp.controller('profileCtrl', function ($scope, $http) {
     $scope.bg = "#C25B56";
-    });
+
+    $http({
+            method: 'GET',
+            url: 'https://api.backand.com:443/1/objects/users',
+            headers: {
+                'AnonymousToken': '9be2ef1b-0af7-4582-a0cb-45b0a8667739'
+            },
+            params: {
+                pageSize: 20,
+                pageNumber: 1,
+                sort: ''
+            }
+        })
+        .then(function (response) {
+                $scope.users = response.data.data;
+            },
+            function errorCallback(response) {
+                alert('Connection to server failed! Try Again');
+
+            });
+});
+
+
+
 //profile controller ends
 
 //error controller starts
